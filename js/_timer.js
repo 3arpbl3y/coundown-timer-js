b217bb114c84728b34dcdad054c5084d12b077f9
3arpbl3y <3arpbl3y@gmail.com>
2022-04-26 22:25:11 +0500

    work done. need to review and refactoring

--- js/_timer.js
+++ js/_timer.js
@@ -3,25 +3,74 @@ export default class Timer {
     root.innerHTML = Timer.getHTML();
 
     this.el = {
-      minutes: root.querySelector("timer__part--minutes"),
-      seconds: root.querySelector("timer__part--seconds"),
-      control: root.querySelector("timer__btn--control"),
-      reset: root.querySelector("timer__btn--reset"),
+      minutes: root.querySelector(".timer__part--minutes"),
+      seconds: root.querySelector(".timer__part--seconds"),
+      control: root.querySelector(".timer__btn--control"),
+      reset: root.querySelector(".timer__btn--reset"),
     };
 
     this.interval = null;
-    this.remainingSeconds = 90;
+    this.remainingSeconds = 0;
 
-    this.updateInterfaceTime();
+    this.el.control.onclick = () => {
+      if (this.interval === null) {
+        this.start();
+      } else {
+        this.stop();
+      }
+    };
 
-    this.el.control.onclick = () => {};
+    this.el.reset.onclick = () => {
+      const inputMinutes = prompt("?");
 
-    this.el.reset.onclick = () => {};
+      if (inputMinutes < 60) {
+        this.stop();
+        this.remainingSeconds = inputMinutes * 60;
+        this.updateInterfaceTime();
+      }
+    };
   }
 
   updateInterfaceTime() {
     const minutes = Math.floor(this.remainingSeconds / 60);
     const seconds = this.remainingSeconds % 60;
+
+    this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+    this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+  }
+
+  updateInterfaceControls() {
+    if (this.interval === null) {
+      this.el.control.innerHTML = `<span class="material-symbols-outlined">play_arrow</span>`;
+      this.el.control.classList.add("timer__btn--start");
+      this.el.control.classList.remove("timer__btn--stop");
+    } else {
+      this.el.control.innerHTML = `<span class="material-symbols-outlined">pause</span>`;
+      this.el.control.classList.add("timer__btn--stop");
+      this.el.control.classList.remove("timer__btn--start");
+    }
+  }
+
+  start() {
+    if (this.remainingSeconds === 0) {
+      return;
+    }
+
+    this.interval = setInterval(() => {
+      this.remainingSeconds--;
+      this.updateInterfaceTime();
+      if (this.remainingSeconds === 0) {
+        this.stop();
+      }
+    }, 1000);
+
+    this.updateInterfaceControls();
+  }
+  stop() {
+    clearInterval(this.interval);
+
+    this.interval = null;
+    this.updateInterfaceControls();
   }
 
   static getHTML() {
@@ -42,3 +91,5 @@ export default class Timer {
 	  `;
   }
 }
+
+new Timer(document.querySelector(".timer"));
